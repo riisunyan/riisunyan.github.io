30d17deccf9d62e99557c20aca0b2086390cee73
riisunyan <reeseparker2001@gmail.com>
2025-02-04 17:14:23 +0000

    javascript changes

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const header = document.createElement("header");
+header.innerHTML = `
+    <div>
+    <button id="nav_button">
+      <svg height="60" width="70px">
+        <rect fill="#00E1E1" height="7px" rx="3" ry="3" width="45px" x="12.5" y="15"></rect>
+        <rect fill="#00E1E1" height="7px" rx="3" ry="3" width="45px" x="12.5" y="30"></rect>
+        <rect fill="#00E1E1" height="7px" rx="3" ry="3" width="45px" x="12.5" y="45"></rect>
+      </svg>
+    </button>
+    <div class="home_bar_items">
+      <a href="/"><img alt="Ichika logo/home button" class="header_home_button" src="/images/favicon.ico"></a>
+      <a href="/"><span>Ichika</span></a>
+    </div>
+  </div>
+`;
+document.body.appendChild(header);
+
+
+const sidebar = document.createElement("nav");
+sidebar.innerHTML = `
+   <div class="nav_content menu_container">
+        <span>Navigation</span>
+    </div>
+    <a href="/figures">
+        <div class="nav_content"><span>Figures</span></div>
+    </a>
+    <a href="/python">
+        <div class="nav_content"><span>Python</span></div>
+    </a>
+    <a href="/chemistry">
+        <div class="nav_content"><span>Chemistry</span></div>
+    </a>
+    <a href="/physics">
+        <div class="nav_content"><span>Physics</span></div>
+    </a>
+    <a href="/maths">
+        <div class="nav_content"><span>Maths</span></div>
+    </a>
+    <svg height="5" width="100%">
+        <rect fill="#002424" height="3" rx="3" ry="3" width="100%"></rect>
+    </svg>
+    <div class="nav_content menu_container" style="display: none">
+        <span>Page Links</span>
+    </div>
+    <p>Only the Homepage, figures page and chemistry page exist.</p>
+`;
+document.body.appendChild(sidebar);
+
+
 document.getElementById('nav_button').addEventListener('click', function () {
     const nav = document.querySelector('nav');
     const body = document.querySelector('body');
@@ -14,6 +64,7 @@ document.getElementById('nav_button').addEventListener('click', function () {
         body.style.paddingLeft = '0';
     }
 });
+
 
 window.addEventListener('resize', function () {
     const body = document.querySelector('body');
